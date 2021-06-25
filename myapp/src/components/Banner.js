2969c5a8cab2db34f6af30b528f7c72d9d5ff371
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-06-25 14:28:24 -0300

    fix header

--- myapp/src/components/Banner.js
+++ myapp/src/components/Banner.js
@@ -24,12 +24,23 @@ export const Banner = () => {
                   </ul>
                   <h1>I am Gabriel Lovera</h1>
                   <p>
-                      im Gabriel, student at UTN San Francisco and tech entusiactic
+                    im Gabriel, student at UTN San Francisco and tech
+                    entusiactic
                   </p>
+                  <div className="header__buttons">
+                    <a href="" className="btn btn-outline">
+                      My portfolio
+                    </a>
+                    &nbsp;&nbsp;&nbsp;
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+            <div className="banner__img">
+              <img src="images/develop.png" alt="dev" />
+            </div>
+            </div>
           </div>
         </div>
       </header>
