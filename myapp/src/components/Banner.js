215c880c74ec5a5c8e5ee45578698103889a453f
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-06-25 01:38:38 -0300

    add icons

--- myapp/src/components/Banner.js
+++ myapp/src/components/Banner.js
@@ -1,11 +1,37 @@
 import React from 'react'
-
+import {FaFacebookF,FaPinterest,FaTwitch,FaLinkedin,FaTwitter,FaInstagram} from "react-icons/fa"
 export const Banner = () => {
     return (
-        <header className="header">
-                
-        </header>
-    )
+      <header className="header">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <div className="header__content">
+                <ul className="header__ul">
+                  <li>
+                    <FaFacebookF />
+                  </li>
+                  <li>
+                    <FaTwitter />
+                  </li>
+                  <li>
+                    <FaPinterest />
+                  </li>
+                  <li>
+                    <FaInstagram />
+                  </li>
+                  <li>
+                    <FaLinkedin />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </header>
+    );
+
 }
 
 export default Banner;
